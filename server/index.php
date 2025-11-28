b533aed947866a37555d4b3eabde41c824700620
Zahraa Najdi <zahraanajdi15@gmail.com>
2025-11-28 11:47:39 +0200

    Create index.php

---
+++ server/index.php
@@ -0,0 +1,36 @@
+<?php 
+require_once("./services/ResponseService.php");
+include(__DIR__ . "/routes/routes.php");
+
+
+$base_dir = rtrim(dirname($_SERVER['SCRIPT_NAME']), '/');
+$request = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
+
+if (strpos($request, $base_dir) === 0) {
+    $request = substr($request, strlen($base_dir));
+}
+
+if ($request == '') {
+    $request = '/';
+}
+
+$apis = $routes;
+
+if (isset($apis[$request])) {
+    $controller_name = $apis[$request]['controllers']; 
+    $method = $apis[$request]['method'];
+    if($controller_name == 'AiAnalyze.php') {
+        require_once "Ai/{$controller_name}";
+            exit;
+    }
+    require_once "controllers/{$controller_name}.php";
+    
+    $controller = new $controller_name();
+    if (method_exists($controller, $method)) {
+        $controller->$method();
+    } else {
+        echo ResponseService::response(500, "Error: Method {$method} not found in {$controller_name}");
+    }
+} else {
+    echo ResponseService::response(404, "Route Not Found");
+}
